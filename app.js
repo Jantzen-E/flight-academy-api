3ad482d53b143b4c133b59e82a968ee8ab84cda3
Jantzen-E <jantzen.egan@gmail.com>
2020-05-30 02:22:23 -0600

    add connect and find functions to DAL and add get by id and get all functions to app.js

--- app.js
+++ app.js
@@ -3,24 +3,48 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const DAL = require('./dataAccessLayer');
+const ObjectId = require('mongodb').ObjectId;
+var cors = require('cors');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 require('dotenv').config();
 var app = express();
+DAL.Connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Get all endpoint
+app.get('/api/schedules', cors(), async function(req, res) {
+  const result = await DAL.Find();
+  res.send(result);
+});
+
+// get by id endpoint
+app.get('/api/schedules/:id', cors(), async function(req, res) {
+  const id = req.params.id;
+  const schedule = {
+    _id: ObjectId(id)
+  };
+  const result = await DAL.Find(schedule);
+  if (result) {
+    res.send(result);
+  }
+  else {
+    res.send('No schedule with ID: ' + id + ' found!');
+  }
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ dataAccessLayer.js
@@ -0,0 +1,47 @@
+const MongoClient = require('mongodb').MongoClient;
+const databaseName = 'scheduling-db';
+const collectionName = 'schedules';
+const mongoDbUrl = process.env.ATLAS_CONNECTION;
+const settings = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+let database;
+
+const Connect = function() {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(mongoDbUrl, settings, function(err, client) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                console.log('successfully connected to database');
+                database = client.db(databaseName);
+                resolve();
+            }
+        });
+    });
+ };
+
+const Find = function(item) {
+    let itemQuery = {};
+    if (item) {
+        itemQuery = item;
+    }
+    console.log(item)
+    return new Promise((resolve, reject) => {
+        const scheduleCollection = database.collection(collectionName);
+        scheduleCollection.find(itemQuery).toArray(function(err, res) {
+            if(err) {
+                reject(err);
+            }
+            else {
+                console.log('successfully found schedules');
+                resolve(res);
+            }
+        });
+    });
+};
+
+module.exports = { Connect, Find };
